5974b8f5758ba23f0f642978cc4bf7941707b1c8
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-04-04 14:47:38 +0200

    fix index.js

--- pages/index.js
+++ pages/index.js
@@ -50,13 +50,45 @@ export default function Home() {
 
         <main className="container mx-auto py-6 px-4 sm:px-6 lg:px-8">
           <h2 className="text-2xl font-bold mb-4">Welcome to the Course!</h2>
-          <p>
-            This is a comprehensive course on Web3 and Cryptocurrencies.
-            Throughout this course, you will learn about blockchain technology,
-            smart contracts, decentralized applications, and much more.
+          <p className="text-lg mb-4">
+            The world is witnessing a paradigm shift in technology and finance.
+            As we enter the era of decentralization, it's crucial to understand
+            the concepts and technologies that are shaping our future. This
+            comprehensive course on Web3 and Cryptocurrencies is designed to
+            provide you with the knowledge and skills needed to navigate and
+            thrive in this exciting new landscape.
           </p>
-        </main>
-      </div>
-    </div>
+          <p className="text-lg mb-4">
+            Our course covers a wide range of topics, from the fundamentals of
+            blockchain technology to the intricacies of smart contracts and
+            decentralized applications. By completing this course, you'll gain a
+            solid understanding of the core concepts and practical applications
+            of Web3 and Cryptocurrencies.
+          </p>
+          <p className="text-lg mb-4">
+            Throughout your learning journey, you'll also have the opportunity
+            to interact with a vibrant community of like-minded individuals
+            passionate about decentralized technology. By engaging with your
+            peers, you'll gain valuable insights and perspectives while building
+            lasting connections that can help you succeed in the rapidly
+            evolving world of Web3.
+          </p>
+          <p className="text-lg mb-4">
+            We firmly believe that education is the key to unlocking the
+            potential of decentralized technology. By equipping yourself with the
+            knowledge and skills offered in this course, you'll be better
+            prepared to embrace the opportunities and challenges that come with the Web3 revolution. As a course participant, you'll be empowered to contribute to the decentralized ecosystem, innovate with new technologies, and ultimately shape the future of the digital world.
+          </p>
+          <p className="text-lg mb-4">
+            We invite you to join us on this exciting journey as we explore the world of Web3 and cryptocurrencies together. Whether you're a beginner looking to get started or an experienced professional seeking to deepen your understanding, this course offers a comprehensive and engaging learning experience tailored to your needs.
+          </p>
+          <p className="text-lg mb-4">
+          Don't miss out on this opportunity to become a part of the decentralized revolution. Embark on your learning journey with us today and unlock the future of technology and finance!
+          </p>
+    </main>
+  </div>
+</div>
   );
 }
+
+export default HomePage;
