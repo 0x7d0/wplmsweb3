735e48925abca86c2b5a72e2c98a1a5cf4cf69a2
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-04-04 17:31:50 +0200

    anoterh update

--- pages/index.js
+++ pages/index.js
@@ -24,67 +24,37 @@ function HomePage() {
       </Head>
 
       <div className="min-h-screen bg-gradient-to-r from-blue-500 to-blue-700 flex">
-        <div className="w-60 p-6 bg-white shadow-xl">
-          <h2 className="text-lg font-medium text-gray-900 mb-3">Table of Contents</h2>
-          <nav>
-            <ul className="space-y-1">
-              {navigation.map((item) => (
-                <li key={item.name}>
-                  <Disclosure>
-                    {({ open }) => (
-                      <>
-                        <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-gray-900 bg-gray-100 rounded-lg hover:bg-gray-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-50">
-                          <span>{item.name}</span>
-                          <ChevronDownIcon
-                            className={classNames(open ? '-rotate-180' : 'rotate-0', 'w-5 h-5 text-gray-500')}
-                          />
-                        </Disclosure.Button>
-                        <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                          <ul>
-                            {item.children.map((child) => (
-                              <li key={child}>
-                                <a href="#" className="block hover:text-gray-800">{child}</a>
-                              </li>
-                            ))}
-                          </ul>
-                        </Disclosure.Panel>
-                      </>
-                    )}
-                  </Disclosure>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
-
-        <div className="flex-1 py-10 px-4 sm:px-6 lg:px-8">
-          <div className="sm:hidden">
-            <div className="pt-2 pb-3">
-              <button
-                type="button"
-                className="bg-blue-600 rounded-md p-2 inline-flex items-center justify-center text-white hover:text-white hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                onClick={() => setIsOpen(!isOpen)}
-              >
-                <span className="sr-only">Open main menu</span>
-                <ChevronDownIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-            </div>
+        <nav className="flex flex-col w-64 bg-gray-800 h-screen py-4 px-2">
+          <div className="text-white font-bold text-2xl mx-auto my-4">Table of Contents</div>
+          <div className="flex flex-col space-y-2">
+            {navigation.map((item) => (
+              <Disclosure key={item.name}>
+                {({ open }) => (
+                  <>
+                    <Disclosure.Button className="w-full flex items-center justify-between bg-gray-900 hover:bg-gray-700 text-white py-2 px-4 rounded-md focus:outline-none focus:ring">
+                      {item.name}
+                      <ChevronDownIcon
+                        className={classNames(open ? 'transform rotate-180' : '', 'h-5 w-5 text-white')}
+                      />
+                    </Disclosure.Button>
+                    <Disclosure.Panel className="bg-gray-900 pb-2 pt-0">
+                      {item.children.map((child) => (
+                        <a
+                          key={child}
+                          href="#"
+                          className="block px-5 py-2 text-sm text-gray-400 hover:text-white hover:bg-gray-700"
+                        >
+                          {child}
+                        </a>
+                      ))}
+                    </Disclosure.Panel>
+                  </>
+                )}
+              </Disclosure>
+            ))}
           </div>
+        </nav>
 
-          <div className="hidden sm:block">
-            <nav className="flex space-x-4">
-              {navigation.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  {item.name}
-                </a>
-              ))}
-            </nav>
-          </div>
-        </div>
         <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
           <div className="text-center">
             <h2 className="text-4xl tracking-tight font-extrabold text-white sm:text-5xl md:text-6xl">
@@ -96,11 +66,34 @@ function HomePage() {
               up-to-date with the latest trends and developments. Our comprehensive course is designed to provide you
               with the knowledge and skills you need to navigate the complex world of Web3, digital assets, and
               decentralized finance with confidence. By taking this course, you'll not only gain a solid understanding
-              of the fundamental concepts but also learn how to put them into practice, enabling you to make informed
-              decisions and seize new opportunities in this exciting space.
-            </p>
+            of the fundamental concepts but also learn how to put them into practice, enabling you to make informed
+            decisions and seize new opportunities in this exciting space.
+          </p>
+          <p className="mt-5 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-8 md:text-xl">
+            Ready to get started? Enroll today and gain the skills and knowledge you need to succeed in the world of
+            Web3 and cryptocurrencies.
+          </p>
+          <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
+            <div className="rounded-md shadow">
+              <Link href="/enroll">
+                <a
+                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
+                >
+                  Enroll now
+                </a>
+              </Link>
+            </div>
+            <div className="mt-3 sm:mt-0 sm:ml-3">
+              <Link href="/about">
+                <a
+                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-600 bg-blue-50 hover:bg-blue-100 md:py-4 md:text-lg md:px-10"
+                >
+                  Learn more
+                </a>
+              </Link>
+            </div>
           </div>
-        </main>
+        </div>
       </div>
     </div>
   );
