71013d4b1513b90ff027a1cd12d5e5001fe0cb96
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-04-04 12:16:50 +0200

    nav to the left

--- pages/index.js
+++ pages/index.js
@@ -2,60 +2,61 @@ import Head from 'next/head';
 import { useState } from 'react';
 
 export default function Home() {
-  const [showNav, setShowNav] = useState(false);
+  const [activeSection, setActiveSection] = useState(null);
 
-  const toggleNav = () => {
-    setShowNav(!showNav);
+  const toggleSection = (section) => {
+    setActiveSection(activeSection === section ? null : section);
   };
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen">
       <Head>
         <title>Learn Web3 & Cryptocurrencies</title>
         <meta name="description" content="Unlock the Future of Decentralized Technology" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="bg-white shadow-sm">
-        <div className="container mx-auto py-4 px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between items-center">
-            <h1 className="text-2xl font-bold">Learn Web3 & Cryptocurrencies</h1>
-            <button onClick={toggleNav} className="text-xl font-bold">
-              ☰
+      <nav className="bg-blue-500 w-64 space-y-4 p-4">
+        <h1 className="text-2xl font-bold text-white">Course Sections</h1>
+        <ul>
+          <li>
+            <button
+              className="font-semibold text-white"
+              onClick={() => toggleSection(1)}
+            >
+              1. Introduction
             </button>
-          </div>
-        </div>
-      </header>
-
-      <main className="container mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <div className="flex">
-          {showNav && (
-            <nav className="bg-gray-200 p-4 w-64 space-y-4">
-              <h2 className="text-xl font-bold">Course Sections</h2>
-              <ul>
+            {activeSection === 1 && (
+              <ul className="pl-4 space-y-1">
                 <li>
-                  <a href="#section1">1. Introduction</a>
+                  <a href="#section1_1" className="text-white">1.1. What is Web3?</a>
                 </li>
-                <li>
-                  <a href="#section2">2. Blockchain Basics</a>
-                </li>
-                <li>
-                  <a href="#section3">3. Smart Contracts</a>
-                </li>
-                {/* Add more sections here */}
+                {/* Add more sub-sections here */}
               </ul>
-            </nav>
-          )}
+            )}
+          </li>
+          {/* Add more sections here */}
+        </ul>
+      </nav>
 
-          <div className="flex-1">
-            <h2 className="text-2xl font-bold mb-4">Welcome to the Course!</h2>
-            <p>
-              This is a comprehensive course on Web3 and Cryptocurrencies. Throughout this course, you will learn about
-              blockchain technology, smart contracts, decentralized applications, and much more.
-            </p>
+      <div className="flex flex-col w-full">
+        <header className="bg-blue-500 py-4">
+          <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+            <h1 className="text-2xl font-bold text-white">
+              Learn Web3 & Cryptocurrencies
+            </h1>
           </div>
-        </div>
-      </main>
+        </header>
+
+        <main className="container mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h2 className="text-2xl font-bold mb-4">Welcome to the Course!</h2>
+          <p>
+            This is a comprehensive course on Web3 and Cryptocurrencies.
+            Throughout this course, you will learn about blockchain technology,
+            smart contracts, decentralized applications, and much more.
+          </p>
+        </main>
+      </div>
     </div>
   );
 }
