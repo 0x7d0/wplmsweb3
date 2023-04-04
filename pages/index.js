14dca4777cedb12719909c0d5d7d9d49a367c89a
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-04-04 11:38:31 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,41 @@
-import React from 'react';
 import Head from 'next/head';
 
-const HomePage = () => {
+export default function Home() {
   return (
-    <div className="bg-gray-100 min-h-screen">
+    <div className="min-h-screen bg-white">
       <Head>
-        <title>My Course</title>
-        <meta name="description" content="Learn Web3 & Cryptocurrencies" />
+        <title>Our Course</title>
+        <meta name="description" content="Learn Web3 and Cryptocurrencies" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="bg-blue-500 py-6">
+      <header className="bg-gradient-to-r from-blue-600 to-blue-500 py-6">
         <div className="container mx-auto px-4">
-          <h1 className="text-4xl font-bold text-white">Learn Web3 & Cryptocurrencies</h1>
-          <p className="text-xl text-white">Unlock the Future of Decentralized Technology</p>
+          <div className="flex items-center justify-between">
+            <div className="text-white font-bold text-2xl">Our Course</div>
+            <nav>
+              <ul className="flex space-x-4">
+                <li>
+                  <a href="#" className="text-white">Home</a>
+                </li>
+                <li>
+                  <a href="#" className="text-white">Courses</a>
+                </li>
+                <li>
+                  <a href="#" className="text-white">Contact</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </header>
 
       <main className="container mx-auto px-4 py-12">
-        <h2 className="text-3xl font-semibold mb-4">Course Outline</h2>
-        <ul className="list-disc list-inside">
-          <li className="mb-2">
-            <a href="#" className="text-blue-600 hover:text-blue-800">1. Introduction to Web3 and Cryptocurrencies</a>
-          </li>
-          <li className="mb-2">
-            <a href="#" className="text-blue-600 hover:text-blue-800">2. Blockchain Fundamentals</a>
-          </li>
-          {/* Add more list items here */}
-        </ul>
+        <h1 className="text-4xl font-bold mb-8">Welcome to Our Course!</h1>
+        <p className="text-lg">
+          Learn about Web3, cryptocurrencies, and more in our comprehensive course. As you progress through the course, you'll unlock new features and build your own NFT character!
+        </p>
       </main>
     </div>
   );
-};
-
-export default HomePage;
+}
